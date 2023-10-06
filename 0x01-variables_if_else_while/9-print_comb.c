ef34424888db0862c10b7bd0fd89ebf79095aa40
elmounkiramine <medamine.mnk@gmail.com>
2023-10-06 13:38:58 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + 48);
+if (digit != 9)
+putchar(',');
+putchar(' ');
+}
+digit++;
+}
+putchar('\n');
+return (0);
+}
