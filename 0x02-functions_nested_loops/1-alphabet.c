6158d3828dad125aaf8fc4bf64eaa34734e6f3c3
elmounkiramine <medamine.mnk@gmail.com>
2023-10-10 12:59:23 -0700

    1-alphabet.cup

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
 
 void print_alphabet(void)
 {
-	char letter;
+	char ch;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
 
 	_putchar('\n');
 }
