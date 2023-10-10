abbc6fd7886ccf412cd3a5cc10606ae5140cc37f
elmounkiramine <medamine.mnk@gmail.com>
2023-10-10 12:35:02 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: Always  0 (success)
+ */
+
+int main(void)
+{
+	char str[] - "_putchar";
+	int ch;
+	for (ch = 0; ch < 8; chi++)
+		_putchar(str[ch]);
+	_putchar('\n');
+	return (0);
